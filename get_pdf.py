e6af5b4e679c6d8aa621cdef16ceabc9b58e3fc8
Trung Le <jackblk4@gmail.com>
2021-03-18 17:54:08 +0700

    feat: beautify everything
    use a specific version for release action to avoid being triggered by github-action update
    
    use the direct link to PDF file
    print more logs, because i like logs

--- get_pdf.py
+++ get_pdf.py
@@ -4,14 +4,17 @@ import requests
 # default_url = "https://jackblk.github.io/my_cv"
 response_tunnel = requests.get("http://localhost:4040/api/tunnels").json()
 url_tunnel = response_tunnel["tunnels"][0]["public_url"]
+print(f"\nURL Tunnel is: '{url_tunnel}'.")
 url = (
     "https://www.web2pdfconvert.com/convert/web/to/pdf?storefile=true&"
     "filename=LeThienTrung_CV"
     f"&url={url_tunnel}"
-    "&Zoom=1&FixedElements=absolute&ViewportWidth=1366&ViewportHeight=800&PageOrientation=portrait&PageSize=a4&MarginLeft=0&MarginRight=0"
+    "&Zoom=1&FixedElements=absolute&ViewportWidth=1366&ViewportHeight=800&"
+    "PageOrientation=portrait&PageSize=a4&MarginLeft=0&MarginRight=0"
 )
+print(f"\nConverting to PDF from: '{url}'.")
 response = requests.get(url).json()
 pdf_link = response["Files"][0]["Url"]
-print(pdf_link)
-print(f"URL Tunnel is: {url_tunnel}")
+print(f"Downloading converted PDF file: '{pdf_link}'.")
 os.system(f"wget {pdf_link} -c")
+print("Downloaded PDF file.")
