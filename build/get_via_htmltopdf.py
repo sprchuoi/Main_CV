ac49091519ec2653db3f4132945e26c5073280f0
kiemhung95 <kiemhung95@gmail.com>
2024-07-11 02:01:38 +0700

    trigger build

--- build/get_via_htmltopdf.py
+++ build/get_via_htmltopdf.py
@@ -5,7 +5,6 @@ from api import HtmlToPdfDotNet
 FILENAME = "NguyenKiemHung_CV.pdf"
 FILENAME_LOCAL = "my_cv_repo/build/CV_Local.pdf"
 
-
 git.Repo.clone_from('https://github.com/kiemhung95/my_cv.git', 'my_cv_repo')
 # response_tunnel = requests.get("http://localhost:4040/api/tunnels").json()
 # url = response_tunnel["tunnels"][0]["public_url"] + "/docs/"
