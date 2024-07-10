09e538d1c87f2fadf50e6b0a9041934108f1e138
kiemhung95 <kiemhung95@gmail.com>
2024-07-11 01:14:59 +0700

    trigger build

--- build/get_via_htmltopdf.py
+++ build/get_via_htmltopdf.py
@@ -3,11 +3,11 @@ from api import HtmlToPdfDotNet
 
 FILENAME = "NguyenKiemHung_CV.pdf"
 FILENAME_LOCAL = "CV_Local.pdf"
-# response_tunnel = requests.get("http://localhost:4040/api/tunnels").json()
-# url = response_tunnel["tunnels"][0]["public_url"] + "/docs/"
-# print(f"\nURL Tunnel is: '{url}'.")
-# # url = "https://www.html-to-pdf.net" # debug only
-# print(f"\nConverting to PDF from: '{url}'.")
+response_tunnel = requests.get("http://localhost:4040/api/tunnels").json()
+url = response_tunnel["tunnels"][0]["public_url"] + "/docs/"
+print(f"\nURL Tunnel is: '{url}'.")
+# url = "https://www.html-to-pdf.net" # debug only
+print(f"\nConverting to PDF from: '{url}'.")
 # pdf_content = HtmlToPdfDotNet().convert_pdf(url)
 with open(FILENAME, "wb") as firstfile, open(FILENAME_LOCAL, "rb") as secondfile:
     # read content from second file 
